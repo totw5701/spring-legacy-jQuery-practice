0d623e9c673088958dc5e992ef0b4063a0ef1f00
DA-WIT <totw5701@naver.com>
2022-03-15 10:53:15 +0900

    Mybatis 연동

--- src/main/java/com/springlegacy/app/HomeController.java
+++ src/main/java/com/springlegacy/app/HomeController.java
@@ -6,11 +6,15 @@ import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.springlegacy.app.service.UserService;
 
 /**
  * Handles requests for the application home page.
@@ -19,6 +23,9 @@ import org.springframework.web.servlet.ModelAndView;
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	@Autowired
+	private UserService userService;
 	
 	/**
 	 * Simply selects the home view to render by returning its name.
@@ -45,6 +52,15 @@ public class HomeController {
 		mv.setViewName("pract1");
 		
 		return mv;
+	}
+	
+	@RequestMapping(value = "/user", method = RequestMethod.GET)
+	@ResponseBody
+	public String user() {
+		
+		String str = userService.testCall();
+		
+		return str;
 	}
 	
 }
---
+++ src/main/java/com/springlegacy/app/service/UserService.java
@@ -0,0 +1,9 @@
+package com.springlegacy.app.service;
+
+public class UserService {
+	
+	public String testCall() {
+		return "hello";
+	}
+
+}
